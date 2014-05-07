e7bd9e5d3246b53937d1a8258f89c19a6b4ca208
Owen Hindley <owenhindley@hotmail.com>
2014-05-07 15:51:08 +0100

    update social footer

--- deploy/socialFooter.php
+++ deploy/socialFooter.php
@@ -2,21 +2,27 @@
 
 
 	<div class="socialButton" id="youtube">
-		<a href="http://youtube.com" target="_blank"><img src="images/youtube.svg" height="20"/></a>
+		<a href="https://www.youtube.com/user/ChristianDeeMusic" target="_blank"><img src="images/youtube.svg" height="20"/></a>
 	</div>
 
 
 	<div class="socialButton" id="soundcloud">
-		<a href="http://soundcloud.com" target="_blank"><img src="images/soundcloud.svg" height="20"/></a>
+		<a href="https://soundcloud.com/christiandee" target="_blank"><img src="images/soundcloud.svg" height="20"/></a>
 	</div>
 
 	<div class="socialButton" id="twitter">
-		<a href="http://twitter.com" target="_blank"><img src="images/twitter.svg" height="20"/></a>
+		<a href="https://twitter.com/CDeeMusic" target="_blank"><img src="images/twitter.svg" height="20"/></a>
 	</div>
-
 
 	<div class="socialButton" id="facebook">
+		<a href="https://www.facebook.com/ChristianDeeMusic" target="_blank"><img src="images/facebook.svg" height="20"/></a>
+	</div>
+
+
+	<div class="socialButton" id="facebookLike">
 		<div class="fb-like" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
 	</div>
+
+	
 
 </div>
